91a90da7f1e738a2797bf72337dac9ad9345c171
Can-Guo <12032421@mail.sustech.edu.cn>
2021-07-14 19:51:39 +0800

    first commit of Wit sensor JY61

---
+++ test_read_imu_2.py
@@ -0,0 +1,17 @@
+# A
+
+'''
+Filename: /home/guoyucan/Downloads/Python_imu_20210714/test_read_imu_2.py
+Path: /home/guoyucan/Downloads/Python_imu_20210714
+Created Date: Wednesday, July 14th 2021, 7:33:31 pm
+Author: guoyucan
+
+'''
+
+## read the data from JY61 for 100 times (calculate the mean value of the x,y,z-axis posture angles)
+
+
+import WitSensor
+
+WitSensor.read_imu_angle_100()
+
